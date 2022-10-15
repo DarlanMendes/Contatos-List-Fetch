576d0a5d5d8a091bc330a50f6a992e254ccdb3ca
Darlan Mendes <dedsondarlan@gmail.com>
2022-10-15 11:24:12 -0300

    Atualização dos métodos de put, post e delete

--- eventos.js
+++ eventos.js
@@ -1,4 +1,5 @@
-let url='https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa';
+//let url='https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa';
+let url='https://634ab69d33bb42dca4099305.mockapi.io/contact'
 atualizacaoCard.style="display:none";
 const addContact=async()=>{
     console.log('nome',nome.value,'idade',idade.value)
@@ -13,48 +14,57 @@ const addContact=async()=>{
     );
     nome.value='';
     idade.value='';
-    console.log(resultado.statusText)
+    console.log(resultado.statusText);
+    atualizarContatos();
 }
 const atualizarContatos=()=>{
+    contatos.innerHTML='Atualizando contatos...'
     fetch(url).
     then(resposta=>resposta.json()).
     then(body=>{
         console.log('dentro',body);
         let conteudoHtml='';
         body.forEach((pessoa,index)=>{
-            conteudoHtml+="<li>"+`${index+1}`+". " +pessoa.nome+" - "+pessoa.idade+" anos <button onclick='deletarPessoa("+pessoa.id+")'>Deletar</button> <button onclick='editarPessoa("+pessoa.id+")'>Atualizar</button></li>" 
+            conteudoHtml+="<li>"+`${index+1}`+". " +pessoa.nome+" - "+pessoa.idade+" anos <button onclick='deletarPessoa("+pessoa.id+")'>Deletar</button> <button onclick='editarPessoa("+pessoa.id+")'>Editar</button></li>" 
         })
         contatos.innerHTML=conteudoHtml;
     })
 }
-const editarPessoa=()=>{
-    atualizacaoCard.style="display:absolute";
+const editarPessoa=(id_pessoa)=>{
+    atualizacaoCard.style="display:fixed";
     atualizacaoCard.style="z-index:5";
+    atualizacaoCard.innerHTML=" <div><input id='novoNome' placeholder='Novo Nome' type='text' /> <input id='novaIdade' placeholder='Nova Idade' type='number'/></div><div><button onclick='atualizarPessoa("+id_pessoa+")'>Atualizar</button><button onclick='cancelarCard()'>Cancelar</button></div>  "
+    console.log(id_pessoa)
 }
 const cancelarCard=()=>{
     atualizacaoCard.style="display:none";
 }
 
-const deletarPessoa=(id_pessoa)=>{
-    fetch(url+"/"+id_pessoa,
+const deletarPessoa= async (id_pessoa)=>{
+    let contatoDeletado = await fetch(url+"/"+id_pessoa,
     {
         method:'DELETE'
     }
 
-    ).then(atualizarContatos())
-   console.log(id_pessoa)
+    )
+   console.log(id_pessoa, contatoDeletado);
+   atualizarContatos();
 }
-const atualizarPessoa=(id_pessoa)=>{
-    fetch(url+"/"+id_pessoa,
+const atualizarPessoa= async (id_pessoa)=>{
+    console.log('teste', novoNome.value)
+   let atualizado = await fetch(url+"/"+id_pessoa,
     {
-        method:'DELETE',
+        method:'PUT',
+        headers:{'Accept': 'application/json',
+        'Content-Type': 'application/json'},
         body:JSON.stringify({
-            nome:nome.value,idade:idade.value
+            nome:novoNome.value,idade:novaIdade.value
         })
     }
-
-    ).then(atualizarContatos())
-   console.log(id_pessoa)
+    
+    )
+atualizarContatos();
+   console.log(atualizado)
 }
 
 document.getElementById("form").addEventListener("click", (event)=>{
