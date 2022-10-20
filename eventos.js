b6c947065b5f62464464a46c87c969dbc523622f
Darlan Mendes <dedsondarlan@gmail.com>
2022-10-20 15:37:01 -0300

    Resolução da passagem de parametro em editar contato

--- eventos.js
+++ eventos.js
@@ -1,19 +1,31 @@
 //let url='https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa';
+let logado = JSON.parse(localStorage.getItem('Logado'));
+console.log(logado)
+
+const fazerLogin=()=>{
+    if(login.value&&senha.value){
+        if(login.value==='login'&&senha.value==='senha'){
+            localStorage.setItem('Logado',JSON.stringify({'login':'login','senha':'senha'}));
+            window.location='index.html'
+        }
+    }else{
+        aviso.innerHTML="Por favor digite todos os campos"
+    }
+}
 let url='https://634ab69d33bb42dca4099305.mockapi.io/contact'
 atualizacaoCard.style="display:none";
 const addContact=async()=>{
-    console.log('nome',nome.value,'idade',idade.value)
     let resultado = await fetch(url,
     {
         method:'POST',
         headers:{'Accept': 'application/json',
         'Content-Type': 'application/json'},
-        body:JSON.stringify({nome:nome.value,idade:idade.value})
+        body:JSON.stringify({nome:nome.value,telefone:telefone.value})
     }
     
     );
     nome.value='';
-    idade.value='';
+    telefone.value='';
     console.log(resultado.statusText);
     atualizarContatos();
 }
@@ -25,16 +37,17 @@ const atualizarContatos=()=>{
         console.log('dentro',body);
         let conteudoHtml='';
         body.forEach((pessoa,index)=>{
-            conteudoHtml+="<li>"+`${index+1}`+". " +pessoa.nome+" - "+pessoa.idade+" anos <button onclick='deletarPessoa("+pessoa.id+")'>Deletar</button> <button onclick='editarPessoa("+pessoa.id+")'>Editar</button></li>" 
+            conteudoHtml+="<li class='d-flex justify-content-between'>"+`${index+1}`+". <h4>" +pessoa.nome+" </h4>  "+pessoa.telefone+"<div> <button class='bg-danger rounded text-white' onclick='deletarPessoa("+pessoa.id+")'><i class='fa-solid fa-trash'></i></button> <button class='bg-primary rounded text-white' onclick='editarPessoa("+index+")'><i class='fa-solid fa-pen'></i></button></div></li>" 
         })
         contatos.innerHTML=conteudoHtml;
     })
 }
-const editarPessoa=(id_pessoa)=>{
+const editarPessoa=(linhaContato)=>{
+    console.log(linhaContato)
     atualizacaoCard.style="display:fixed";
     atualizacaoCard.style="z-index:5";
-    atualizacaoCard.innerHTML=" <div><input id='novoNome' placeholder='Novo Nome' type='text' /> <input id='novaIdade' placeholder='Nova Idade' type='number'/></div><div><button onclick='atualizarPessoa("+id_pessoa+")'>Atualizar</button><button onclick='cancelarCard()'>Cancelar</button></div>  "
-    console.log(id_pessoa)
+    atualizacaoCard.innerHTML="<div class='d-flex position-fixed bg-white mt-5 h-25 p-5 border-dark'><div><input id='novoNome' placeholder='Novo Contato' value='"+nome+"' class='rounded mb-1' type='text' /> <input id='novoTelefone' placeholder='Novo Telefone' value='"+telefone+"' class='rounded mb-1'type='text'/></div> <div class='d-flex align-items-center'><button class='bg-success rounded m-2' onclick='atualizarPessoa("+id+")'><i class='fa-solid fa-upload text-white'></i></button><button class='bg-danger rounded' onclick='cancelarCard()'><i class='fa-solid fa-xmark text-white'></i></button></div></div>"
+    //console.log(id_pessoa)
 }
 const cancelarCard=()=>{
     atualizacaoCard.style="display:none";
@@ -51,20 +64,25 @@ const deletarPessoa= async (id_pessoa)=>{
    atualizarContatos();
 }
 const atualizarPessoa= async (id_pessoa)=>{
-    console.log('teste', novoNome.value)
+    console.log('id pessoa', id_pessoa)
    let atualizado = await fetch(url+"/"+id_pessoa,
     {
         method:'PUT',
         headers:{'Accept': 'application/json',
         'Content-Type': 'application/json'},
         body:JSON.stringify({
-            nome:novoNome.value,idade:novaIdade.value
+            nome:novoNome.value,telefone:novoTelefone.value
         })
     }
     
     )
-atualizarContatos();
-   console.log(atualizado)
+    atualizacaoCard.style="display:none";
+    contatos.innerHTML='Atualizando...'
+    console.log(atualizado);
+    
+await atualizarContatos();
+   
+   
 }
 
 document.getElementById("form").addEventListener("click", (event)=>{
